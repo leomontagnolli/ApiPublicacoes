c47f7e578ce53d9d5093724a8666c995b883c9f1
Leonardo <leomontagnolli@gmail.com>
2020-05-30 20:39:27 -0300

    Implementando entidade usuario

--- src/main/java/com/carol/cadv/model/Usuario.java
+++ src/main/java/com/carol/cadv/model/Usuario.java
@@ -1,5 +1,47 @@
 package com.carol.cadv.model;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Usuario {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String email;
+	private String senha;
+	private String nomeCompleto;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	
+	
+	
 }
+
